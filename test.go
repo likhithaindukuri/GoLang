57b1760c9209624ff1d2008483a15824b427a221
Indukuri Likhitha <likhithaindukuri07@gmail.com>
2025-05-10 21:23:00 +0530

    Loops in GoLang

--- test.go
+++ test.go
@@ -24,4 +24,22 @@ func main() {
 	// const age=21
 	// fmt.Println(age)
 	// // age=22 //throws error that cannot assign value to this
+
+	// //Loops in GoLang->Only one loop that is For loop
+	// //1.Infinite loop
+	// for{
+	// 	fmt.Println("Hello")
+	// }
+
+	// //2.Specifying condition
+	// i:=1
+	// for i<=5{
+	// 	fmt.Println("Hello",i)
+	// 	i++
+	// }
+
+	//3.condition in single line
+	for i:=1;i<=3;i++{
+		fmt.Println("Hello",i)
+	}
 }
