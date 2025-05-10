124668afe02f828608e1792422a92cd544ec00d9
Indukuri Likhitha <likhithaindukuri07@gmail.com>
2025-05-10 21:05:54 +0530

    Variables and Constants in GoLang

--- test.go
+++ test.go
@@ -7,8 +7,21 @@ func main() {
     // fmt.Println("Hello world")// println for new line
 	// fmt.Print("Hello")// print for same line
 
-	//Declaring variables
-	var num1=2
-	var num2=3
-	fmt.Print(num1+num2)
+	// //Declaring variables
+	// var num1=2
+	// var num2=3
+	// fmt.Print(num1+num2)
+
+	// //shortway using :=
+	// age:=21
+	// fmt.Println(age)
+
+	// //multiple variable assigning
+	// x,y,z:=10,20,30
+	// fmt.Println(x,y,z)
+
+	// //constants
+	// const age=21
+	// fmt.Println(age)
+	// // age=22 //throws error that cannot assign value to this
 }
